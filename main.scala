e04c0477b4f8c2abaffe5a67fc55257ad64d92db
corogly <40205088+sabitrayan@users.noreply.github.com>
2020-10-20 00:25:02 +0600

    Add files via upload

--- main.scala
+++ main.scala
@@ -1,54 +1,63 @@
-import java.io.{File, PrintWriter}
-import java.util.Base64.Encoder
-import scala.collection.mutable.ListBuffer
-import scala.io.Source
-import scala.util.matching.Regex
+import akka.actor._
+import language.postfixOps
+import AkkaMessages._
+import ExampleCalc._
 
-object Parser extends App {
-
-  def pars(): Unit = {
-    val file = Source.fromFile("/Users/corogly/Desktop/scala_SIS5/raw.txt").mkString
-
-    case class Product(id: String, name: String, count: String, cost: String, totalPrice: String)
-    case class Check(products: Array[Product])
-
-    var LineCnt = 1
-    var name: String = ""
-    var id: String = ""
-    var count: String = ""
-    var cost: String = ""
-    var totalPrice: String = ""
-
-    var check = Array[Product]()
-
-    for(i <- 0 to file.length) {
-      if (file(i).trim.matches("[0-9]+[.]")) {
-        id = file(i)
-        LineCnt = LineCnt + 1;
-        name = file(i);
-        LineCnt = LineCnt + 1;
-        count = file(i).substring(0, file(i).indexOf("x") + 2)
-        cost = file(i).substring(0, file(i).indexOf("x") - 1)
-        LineCnt = LineCnt + 1;
-        totalPrice = file(i)
-        LineCnt = LineCnt + 3;
-        val product = Product(id, name, count, cost, totalPrice)
-        check = check :+ product
-        LineCnt = LineCnt + 1;
-      }
-    }
-
-    var Json = ListBuffer[String]()
-    for (p <- check) {
-      Json= Encoder[Product].apply(p).toString
-    }
-
-    val pw = new PrintWriter(new File("/Users/corogly/Desktop/scala_SIS5/ans.txt"))
-    for (p <- Json) {
-      pw.write(p + "\n")
-    }
-    pw.close
-  }
-  pars()
+object AkkaComputeApp extends App {
+  val exampleSystem = ActorSystem("exampleSystem")
+  val computeManager = exampleSystem.actorOf(Props[ComputeManager], name = "computing-manager")
+  computeManager ! Compute(inputSet)
 }
 
+class ComputeManager extends Actor with ActorLogging{
+  var results = collection.mutable.Set[Any]()
+  var computingWorker  = collection.mutable.Set[ActorRef]()
+  override def receive: Receive = {
+    case Compute(inputSet) => {
+      inputSet.map{ input =>
+        log.info(s"Creating new worker for $input")
+        val compUnit = context.actorOf(Props(classOf[CompUnit],someCalculationFunc))
+        context.watch(compUnit)
+        computingWorker add compUnit
+        compUnit ! SingleCompute(input)
+      }
+    }
+    case Result(result) => {
+      log.info(s"Got result $result")
+      results add result
+    }
+    case Terminated(someOne) => {
+      log.info(s"Got termination from  $someOne")
+      computingWorker remove someOne
+      if (computingWorker.isEmpty) {
+        log.info(s"finished working, result : $results")
+        context.stop(self)
+      }
+    }
+  }
+}
+
+class CompUnit(compFunc: (Any => Any) ) extends Actor with ActorLogging{
+  override def receive: Receive = {
+    case SingleCompute(input) => {
+      log.info(s"Start to compute $input")
+      sender ! Result(compFunc(input))
+      log.info(s"Finished to compute $input")
+      context.stop(self)
+    }
+  }
+}
+
+object AkkaMessages {
+
+  sealed trait Message
+  case class Compute(inputSet:Iterable[Any]) extends Message
+  case class SingleCompute(input:Any) extends Message
+  case class Result(result:Any) extends Message
+
+}
+
+object ExampleCalc {
+  val someCalculationFunc : (Int) => Int =  _ + 10
+  val inputSet = Range(1,5)
+}
